72954d0e3a0fbcc347795c25bc61eee977e58d3c
veeraaix <veera.aix@gmail.com>
2018-02-21 15:50:45 +0000

    updated println line

--- add2num.java
+++ add2num.java
@@ -5,7 +5,7 @@ class AddNumbers
    public static void main(String args[])
    {
       int x, y, z;
-      System.out.println("Enter two integers to calculate their sum :");
+      System.out.println("Enter two integer numbers to calculate their sum :");
       Scanner in = new Scanner(System.in);
       x = in.nextInt();
       y = in.nextInt();
